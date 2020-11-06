c927ac28aba843de5d406c409f92e5f5a6334b0e
Marco Guidi <mark.88g@gmail.com>
2020-11-06 13:50:43 +0100

    Aggiunto titolo VUE e img VUE

---
+++ assets/main.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: "#root",
+    data: {
+        greeting: "Hello!",
+        coffeeImg: "https://images.unsplash.com/photo-1489533119213-66a5cd877091?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60",
+    }
+
+});
